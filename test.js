6752845dfe5b967b2ba61f33fa942959e228d3a4
anzerr <thoron174+anzerr@gmail.com>
2020-01-24 12:34:25 +0100

    🚧 update tests

--- test.js
+++ test.js
@@ -1,27 +1,34 @@
 
 const copy = require('./index.js'),
+	fs = require('fs.promisify'),
 	hash = require('fs.hash'),
 	remove = require('fs.remove'),
 	assert = require('assert'),
 	promise = require('promise.util');
 
-promise.measure(() => {
-	return copy('./node_modules/@anzerr/fs.remove', './test', {max: 5}).then(() => {
-		return Promise.all([
-			hash('./node_modules/@anzerr/fs.remove', {raw: true, max: 5}),
-			hash('./test', {raw: true, max: 5})
-		]);
-	}).then((res) => {
-		for (let x in res) {
-			for (let i in res[x]) {
-				assert.equal(res[0][i], res[1][i]);
+fs.access('./node_modules/@anzerr/fs.remove').then(() => {
+	return './node_modules/@anzerr/fs.remove';
+}).catch(() => {
+	return './node_modules/fs.remove';
+}).then((file) => {
+	return promise.measure(() => {
+		return copy(file, './test', {max: 5}).then(() => {
+			return Promise.all([
+				hash(file, {raw: true, max: 5}),
+				hash('./test', {raw: true, max: 5})
+			]);
+		}).then((res) => {
+			for (let x in res) {
+				for (let i in res[x]) {
+					assert.equal(res[0][i], res[1][i]);
+				}
 			}
-		}
-		return remove('./test');
-	}).catch((err) => {
-		console.log(err);
-		remove('./test').then(() => {
-			process.exit(1);
+			return remove('./test');
+		}).catch((err) => {
+			console.log(err);
+			remove('./test').then(() => {
+				process.exit(1);
+			});
 		});
 	});
 }).then((res) => {
